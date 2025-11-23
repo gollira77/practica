c7c173157f5cb31d61edd6555b35f6bb34412a78
Franco <franquivillalba13@gmail.com>
2025-11-22 23:20:38 -0300

    Cree los archivos de jwt y bcrypt para hashear la contraseña y crear los tokens con la firma de la variable de entorno

---
+++ src/helpers/jwt.js
@@ -0,0 +1,12 @@
+import e from 'express';
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (payload) => {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: '1h'
+    });
+};
+
+export const verifyToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+};
